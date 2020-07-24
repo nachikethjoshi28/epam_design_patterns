2168d5bb54490c6028ff94cb939608fd4e30ad02
Nachikethjoshi28 <nachikethjoshi2802@gmail.com>
2020-07-24 15:39:46 +0530

    epam design patterns

---
+++ src/main/java/com/epam/DesignPatterns/App.java
@@ -0,0 +1,33 @@
+package com.epam.DesignPatterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	private static App cal ;
+	private App() {};
+	public static App getInstance()
+	{
+		if(cal==null)
+			cal = new App();
+		return cal;
+	}
+	public int addition(int a,int b)
+	{
+		return a+b;
+	}
+	public  int subtraction(int a,int b)
+	{
+		return a-b;
+	}
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+}
